01071fb1a3c990992efa5dbc949f324803356cde
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-10-31 09:30:03 +0100

    Update pokemon.sql

--- pokemon.sql
+++ pokemon.sql
@@ -1,6 +1,6 @@
 create table City (
 	id int primary key,
-    name varchar(45) not null unique
+	name varchar(45) not null unique
 );
 
 create table Trainer (
@@ -31,4 +31,25 @@ create table Trainer_Gym(
     foreign key(gym_id) references Gym(id),
     trainer_id int not null,
 	foreign key(trainer_id) references Trainer(id)
-)
+);
+
+insert into City (id, name)
+values (1, "Yoho"), (2, "Kojuo");
+
+insert into Trainer (id, name, gender)
+values (1, "Albert", "Male"), (2, "Marunka", "Female");
+insert into Trainer (id, name)
+values (3, "Lucius");
+
+insert into Pokemon (id, name, type, level, trainer_id)
+values 
+	(1, "Mew", "Psychic", 690, 3), 
+    (2, "Pikachu", "Electric", 2, 1), 
+    (3, "Lapras", "Water", 25, 2);
+
+
+insert into Gym (id, name, city_id)
+values (1, "Ultimate_Gym", 1);
+
+insert into Trainer_Gym(id, gym_id, trainer_id)
+values (1, 1, 3)
